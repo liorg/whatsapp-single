35ccff611e3bf12b5b6dfc62853f576db18903d8
liorg <lior.grosman@gmail.com>
2026-07-01 09:29:20 +0300

    23

--- baileys/src/index.js
+++ baileys/src/index.js
@@ -15,7 +15,7 @@ import { Boom } from '@hapi/boom';
 import RedisStreams from './redis-streams.js';
 import path from 'path';         // ← 
 const PHONE_ID     = process.env.PHONE_ID || null;  // ← הוסף
-const APP_VERSION = '1.0.0.21';
+const APP_VERSION = '1.0.0.23';
 let pairingCodeData = null;        // ←20 
 const  user_display= process.env.USER_DISPLAY || '****anon';
 const USE_PAIRING_CODE = process.env.USE_PAIRING_CODE === 'true';
